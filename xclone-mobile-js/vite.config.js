fbaf80b23a3c346e018cae80234e2a70e3bd4490
Mulamba Pius <127339680+MarlonPiusMulamba@users.noreply.github.com>
2026-01-09 12:09:16 +0300

    Update vite.config.js

--- xclone-mobile-js/vite.config.js
+++ xclone-mobile-js/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: './',
+    base: '/',
     define: {
       'process.env': { ...env }
     },
